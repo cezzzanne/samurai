bcf81e1cd319c943dc0bff0690beabc9827f47cc
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-10-23 23:41:02 -0700

    1

--- samurai/settings.py
+++ samurai/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
