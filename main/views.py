abe60a32273a7c0009087cfbc0c9743bda9cbfec
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-10-23 23:49:16 -0700

    134

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-from nokia import NokiaAuth, NokiaApi
 from django.shortcuts import render
 
 CLIENT_ID = "a2d850f43357b6fb2ab22a59b431ad3d438a0031d329ecec37686764e49b7939"
@@ -7,9 +6,9 @@ CALLBACK_URI = "http://www.samuray.ml/test"
 
 
 def test(request):
-    auth = NokiaAuth(CLIENT_ID, CONSUMER_SECRET, callback_uri=CALLBACK_URI)
-    authorize_url = auth.get_authorize_url()
-
+    # auth = NokiaAuth(CLIENT_ID, CONSUMER_SECRET, callback_uri=CALLBACK_URI)
+    # authorize_url = auth.get_authorize_url()
+    authorize_url = ""
     return render(request, 'test.html', {"auth_url": authorize_url})
 
 
