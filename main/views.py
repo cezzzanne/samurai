a81e97c899f4aaab853d2f312ea2b903f6a3776b
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-10-24 00:43:40 -0700

    prod testing

--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from nokia import NokiaAuth, NokiaApi
 CLIENT_ID = "a2d850f43357b6fb2ab22a59b431ad3d438a0031d329ecec37686764e49b7939"
 CONSUMER_SECRET = "7b52d21d03a644fe0491d7b0455a46264dfb7d055a36bb5be0ef7afc160c4bf1"
-CALLBACK_URI = "http://127.0.0.1:8000/test/success"
+CALLBACK_URI_TEST = "http://127.0.0.1:8000/test/success"
+CALLBACK_URI = "http://www.samuray.ml/test/success"
 
 
 def test(request):
