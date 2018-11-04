2ca91262a09b37c61880c4833592c8c5bfe277b0
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-11-04 01:43:30 -0800

    test using Whithings API

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import test, test_success
 urlpatterns = [
     path('', test),
-    path('success/', test_success)
+    path('success', test_success)
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from nokia import NokiaAuth, NokiaApi
+import requests
+import time
 CLIENT_ID = "a2d850f43357b6fb2ab22a59b431ad3d438a0031d329ecec37686764e49b7939"
 CONSUMER_SECRET = "7b52d21d03a644fe0491d7b0455a46264dfb7d055a36bb5be0ef7afc160c4bf1"
 CALLBACK_URI_TEST = "http://127.0.0.1:8000/test/success"
@@ -7,15 +9,33 @@ CALLBACK_URI = "http://www.samuray.ml/test/success"
 
 
 def test(request):
-    auth = NokiaAuth(CLIENT_ID, CONSUMER_SECRET, callback_uri=CALLBACK_URI)
+    # TODO: Test is on
+    auth = NokiaAuth(CLIENT_ID, CONSUMER_SECRET, callback_uri=CALLBACK_URI_TEST)
     authorize_url = auth.get_authorize_url()
     return render(request, 'test.html', {"auth_url": authorize_url})
 
 
 def test_success(request):
-    auth = NokiaAuth(CLIENT_ID, CONSUMER_SECRET, callback_uri=CALLBACK_URI)
+    auth = NokiaAuth(CLIENT_ID, CONSUMER_SECRET, callback_uri=CALLBACK_URI_TEST)
     authorization_response = request.GET['code']
     credentials = auth.get_credentials(authorization_response)
     client = NokiaApi(credentials)
-    measures = client.get_measures(limit=1)
-    return render(request, 'success.html', {'var': measures[0].weight})
+    access_token = credentials.access_token
+    meas_type = "6"
+    category = "1"
+    start_date = int(time.time())
+    link = "https://wbsapi.withings.net/measure?action=getmeas&access_token=" + access_token + "&meastype=1&category=1"
+    resp = requests.get(link)
+    measures = client.get_measures()
+    res = resp.json()['body']['measuregrps']
+    res2 = get_weights(res)
+    return render(request, 'success.html', {'var': res2})
+
+
+def get_weights(json_res):
+    weights_array = []
+    for res in json_res:
+        weight = res['measures'][0]['value']
+        real_weight = weight / 100
+        weights_array.append(res)
+    return weights_array
