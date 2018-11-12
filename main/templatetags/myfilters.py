66801fa684da1cd53411b02f98803d8ead54615d
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-11-12 15:37:16 -0800

    graphs

--- main/templatetags/myfilters.py
+++ main/templatetags/myfilters.py
@@ -5,7 +5,10 @@ register = template.Library()
 @register.filter(name='get_weights')
 def get_weights_filter(person):
     # return person.user.username
-    return person.get_weights()
+    if person.access_token is not None:
+        return person.get_weights()
+    else:
+        return []
 
 
 @register.filter(name='get_days')
